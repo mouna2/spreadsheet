7ac1b577d639e1318724cc840c8ba05fd8170788
mouna <mouna@DESKTOP-UAD8M32>
2018-04-20 09:50:19 +0200

    new commit

--- src/com/konv/spreadsheet/Main.java
+++ src/com/konv/spreadsheet/Main.java
@@ -46,7 +46,8 @@ import at.jku.isse.designspace.richapi.model.impl.PropertyTypeArtifactImpl;
 public class Main extends Application {
 	private static Workspace clientConnectionHandler = Workspace.init(InetAddress.getLoopbackAddress().getHostAddress(), null);
 	private static PrimitiveAPI clientConnection = clientConnectionHandler.getClientConnection();
-	
+	  String option1=""; 
+       String option2=""; 
 	
 	//MOUNA
     private SpreadsheetView mSpreadsheet;
@@ -57,6 +58,9 @@ public class Main extends Application {
     private int mColumnCount = 26;
     String artifact1=""; 
     String artifact2=""; 
+    String Combination=""; 
+    boolean Flag1=false; 
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
         initUi(primaryStage);
@@ -76,25 +80,7 @@ public class Main extends Application {
     private void initUi(Stage stage) {
         VBox root = new VBox();
         mTextField = new TextField();
-        mTextField.setOnAction(e -> {
-            int focusedRow = mSpreadsheet.getSelectionModel().getFocusedCell().getRow();
-            int focusedColumn = mSpreadsheet.getSelectionModel().getFocusedCell().getColumn();
-            mSpreadsheet.getGrid().setCellValue(focusedRow, focusedColumn, mTextField.getText());
-        });
-
-        initSpreadsheet();
-        VBox.setVgrow(mSpreadsheet, Priority.ALWAYS);
-
-        ToolBar toolBar = new ToolBar();
-        ToggleButton toggleButton = new ToggleButton("Show Formulas");
-        toggleButton.setSelected(false);
-        toggleButton.selectedProperty().addListener((e, oldValue, newValue) -> {
-
-            mSpreadsheetController.setShowFormulas(newValue);
-            mSpreadsheetController.display();
-        });
-       
-        
+      
         final ChoiceBox<String> box = new ChoiceBox<String>();
 
         box.getItems().add("State");
@@ -105,11 +91,14 @@ public class Main extends Application {
         box.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
           @Override
           public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
-            System.out.println(box.getItems().get((Integer) number2));
+        
+            option1=box.getItems().get((Integer) number2); 
+           // System.out.println(option1);
+          
           }
         });
        
-        
+      
         final ChoiceBox<String> box2 = new ChoiceBox<String>();
 
         box2.getItems().add("State");
@@ -120,11 +109,44 @@ public class Main extends Application {
         box2.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
           @Override
           public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
-            System.out.println(box2.getItems().get((Integer) number2));
+        	  option2=box.getItems().get((Integer) number2); 
+             // System.out.println(option2);
+        	   Combination= option1+"-"+option2; 
+              System.out.println(Combination);
+              if(Combination.equals("Requirement-State")) {
+            	   initSpreadsheet();
+                   VBox.setVgrow(mSpreadsheet, Priority.ALWAYS);
+                   Flag1=false; 
+                   initUi(stage);
+              }
+               if(Combination.equals("Code-StateMachine")) {
+           	      initSpreadsheet2();
+                  VBox.setVgrow(mSpreadsheet, Priority.ALWAYS);
+                  Flag1=true; 
+                  initUi(stage);
+             }
+             
           }
         });
+    
+     
+        mTextField.setOnAction(e -> {
+            int focusedRow = mSpreadsheet.getSelectionModel().getFocusedCell().getRow();
+            int focusedColumn = mSpreadsheet.getSelectionModel().getFocusedCell().getColumn();
+            mSpreadsheet.getGrid().setCellValue(focusedRow, focusedColumn, mTextField.getText());
+        });
+        if(Flag1==false) {
+        	initSpreadsheet();
+            VBox.setVgrow(mSpreadsheet, Priority.ALWAYS);
+        }
+        else {
+        	initSpreadsheet2();
+            VBox.setVgrow(mSpreadsheet, Priority.ALWAYS);
+        }
+      
 
-        toolBar.getItems().addAll(toggleButton);
+        ToolBar toolBar = new ToolBar();
+       
         toolBar.getItems().addAll(box);
         toolBar.getItems().addAll(box2);
 
@@ -275,13 +297,13 @@ public class Main extends Application {
 		link5.setPropertyValue("linktarget", State1);
 		link5.setPropertyValue("linktype", "State-Requirement"); 
 
-		link6.setPropertyValue("linksource", Requirement5);
-		link6.setPropertyValue("linktarget", State3);
+		link6.setPropertyValue("linksource", code4);
+		link6.setPropertyValue("linktarget", statemachine3);
 		link6.setPropertyValue("linktype", "Code-StateMachine"); 
 
 		
-		link7.setPropertyValue("linksource", Requirement7);
-		link7.setPropertyValue("linktarget", State5);
+		link7.setPropertyValue("linksource", code2);
+		link7.setPropertyValue("linktarget", statemachine2);
 		link7.setPropertyValue("linktype", "Code-StateMachine"); 
 		
 		
@@ -497,6 +519,366 @@ public class Main extends Application {
 			
 		}
 		  
+	
+	
+private void initSpreadsheet2() {
+    	
+    	ProjectArtifact sampleProject = new ProjectArtifactImpl("SampleProject");
+		PackageArtifact defaultTypePackage = sampleProject.createPackage();
+		PackageArtifact defaultInstancePackage = sampleProject.createPackage();
+	//	defaultTypePackage.getArtifactCollection();
+		TypeArtifact RequirementType = defaultTypePackage.createType(Set.class.getName());
+		TypeArtifact LinkType = defaultTypePackage.createType(Set.class.getName());
+		TypeArtifact StateType = defaultTypePackage.createType(Set.class.getName());
+		
+		TypeArtifact CodeType = defaultTypePackage.createType(Set.class.getName());
+		TypeArtifact StateMachineType = defaultTypePackage.createType(Set.class.getName());
+		
+		StateMachineType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("statemachinename", Artifact.STRING_TYPE.getArtifactId(), false)));
+		
+		InstanceArtifact statemachine1 = defaultInstancePackage.createInstance(StateMachineType);
+		statemachine1.setPropertyValue("statemachinename", "statemachine1");
+		
+		InstanceArtifact statemachine2 = defaultInstancePackage.createInstance(StateMachineType);
+		statemachine2.setPropertyValue("statemachinename", "statemachine2");
+		
+		InstanceArtifact statemachine3 = defaultInstancePackage.createInstance(StateMachineType);
+		statemachine3.setPropertyValue("statemachinename", "statemachine3");
+		
+		InstanceArtifact statemachine4 = defaultInstancePackage.createInstance(StateMachineType);
+		statemachine4.setPropertyValue("statemachinename", "statemachine4");
+		
+		CodeType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("codename", Artifact.STRING_TYPE.getArtifactId(), false)));
+
+		InstanceArtifact code1 = defaultInstancePackage.createInstance(CodeType);
+		code1.setPropertyValue("codename", "code1");
+		
+		InstanceArtifact code2 = defaultInstancePackage.createInstance(CodeType);
+		code2.setPropertyValue("codename", "code2");
+		
+		InstanceArtifact code3 = defaultInstancePackage.createInstance(CodeType);
+		code3.setPropertyValue("codename", "code3");
+		
+		InstanceArtifact code4 = defaultInstancePackage.createInstance(CodeType);
+		code4.setPropertyValue("codename", "code4");
+		
+		RequirementType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("requirementname", Artifact.STRING_TYPE.getArtifactId(), false)));
+		
+		InstanceArtifact Requirement1 = defaultInstancePackage.createInstance(RequirementType);
+		Requirement1.setPropertyValue("requirementname", "r1");
+		
+		InstanceArtifact Requirement2 = defaultInstancePackage.createInstance(RequirementType);
+		Requirement2.setPropertyValue("requirementname", "r2");
+		
+		InstanceArtifact Requirement3 = defaultInstancePackage.createInstance(RequirementType);
+		Requirement3.setPropertyValue("requirementname", "r3");
+		
+		InstanceArtifact Requirement4 = defaultInstancePackage.createInstance(RequirementType);
+		Requirement4.setPropertyValue("requirementname", "r4");
+		
+		InstanceArtifact Requirement5 = defaultInstancePackage.createInstance(RequirementType);
+		Requirement5.setPropertyValue("requirementname", "r5");
+		
+		InstanceArtifact Requirement6 = defaultInstancePackage.createInstance(RequirementType);
+		Requirement6.setPropertyValue("requirementname", "r6");
+		
+		InstanceArtifact Requirement7 = defaultInstancePackage.createInstance(RequirementType);
+		Requirement7.setPropertyValue("requirementname", "r7");
+		
+		StateType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("statename", Artifact.STRING_TYPE.getArtifactId(), false)));
+		InstanceArtifact State1 = defaultInstancePackage.createInstance(StateType);
+		State1.setPropertyValue("statename", "s1");
+		
+		InstanceArtifact State2 = defaultInstancePackage.createInstance(StateType);
+		State2.setPropertyValue("statename", "s2");
+		
+		InstanceArtifact State3 = defaultInstancePackage.createInstance(StateType);
+		State3.setPropertyValue("statename", "s3");
+		
+		InstanceArtifact State4 = defaultInstancePackage.createInstance(StateType);
+		State4.setPropertyValue("statename", "s4");
+		
+		InstanceArtifact State5 = defaultInstancePackage.createInstance(StateType);
+		State5.setPropertyValue("statename", "s5");
+		
+		
+		LinkType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("linkname", Artifact.STRING_TYPE.getArtifactId(), false)));
+		LinkType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("linktype", Artifact.STRING_TYPE.getArtifactId(), false)));
+		LinkType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("linksource", Artifact.STRING_TYPE.getArtifactId(), false)));
+		LinkType.addPropertyType(new PropertyTypeArtifactImpl(new FeatureDescription("linktarget", Artifact.STRING_TYPE.getArtifactId(), false)));
+		InstanceArtifact link1 = defaultInstancePackage.createInstance(LinkType);
+		link1.setPropertyValue("linkname", "l1");
+		
+		
+		
+		InstanceArtifact link2 = defaultInstancePackage.createInstance(LinkType);
+		link2.setPropertyValue("linkname", "l2");
+		link2.setPropertyValue("linktype", "State-Requirement"); 
+		
+		
+		InstanceArtifact link3 = defaultInstancePackage.createInstance(LinkType);
+		link3.setPropertyValue("linkname", "l3");
+		link3.setPropertyValue("linktype", "State-Requirement"); 
+
+		
+		InstanceArtifact link4 = defaultInstancePackage.createInstance(LinkType);
+		link4.setPropertyValue("linkname", "l4");
+		link4.setPropertyValue("linktype", "State-Requirement"); 
+
+		
+		InstanceArtifact link5 = defaultInstancePackage.createInstance(LinkType);
+		link5.setPropertyValue("linkname", "l5");
+		link5.setPropertyValue("linktype", "State-Requirement"); 
+
+		InstanceArtifact link6 = defaultInstancePackage.createInstance(LinkType);
+		link6.setPropertyValue("linkname", "l6");
+		link6.setPropertyValue("linktype", "State-Requirement"); 
+
+		InstanceArtifact link7 = defaultInstancePackage.createInstance(LinkType);
+		link7.setPropertyValue("linkname", "l7");
+		link7.setPropertyValue("linktype", "State-Requirement"); 
+
+		link1.setPropertyValue("linksource", Requirement1);
+		link1.setPropertyValue("linktarget", State2);
+		link1.setPropertyValue("linktype", "State-Requirement"); 
+
+		link2.setPropertyValue("linksource", Requirement2);
+		link2.setPropertyValue("linktarget", State2);
+		link2.setPropertyValue("linktype", "State-Requirement"); 
+
+		link3.setPropertyValue("linksource", Requirement3);
+		link3.setPropertyValue("linktarget", State3);
+		link3.setPropertyValue("linktype", "State-Requirement"); 
+
+		link4.setPropertyValue("linksource", Requirement1);
+		link4.setPropertyValue("linktarget", State3);
+		link4.setPropertyValue("linktype", "State-Requirement"); 
+
+		link5.setPropertyValue("linksource", Requirement2);
+		link5.setPropertyValue("linktarget", State1);
+		link5.setPropertyValue("linktype", "State-Requirement"); 
+
+		link6.setPropertyValue("linksource", code4);
+		link6.setPropertyValue("linktarget", statemachine3);
+		link6.setPropertyValue("linktype", "Code-StateMachine"); 
+
+		
+		link7.setPropertyValue("linksource", code2);
+		link7.setPropertyValue("linktarget", statemachine2);
+		link7.setPropertyValue("linktype", "Code-StateMachine"); 
+		
+		
+		
+
+		List<InstanceArtifact> LinkList = new ArrayList<InstanceArtifact>(); 
+		
+	
+		List<InstanceArtifact> RequirementList= new ArrayList<InstanceArtifact>(); 
+		RequirementList.add(Requirement1); 
+		RequirementList.add(Requirement2); 
+		RequirementList.add(Requirement3); 
+		RequirementList.add(Requirement4); 
+		RequirementList.add(Requirement5); 
+		RequirementList.add(Requirement6); 
+		RequirementList.add(Requirement7); 
+		
+		
+		List<InstanceArtifact> StateList= new ArrayList<InstanceArtifact>(); 
+		StateList.add(State1); 
+		StateList.add(State2); 
+		StateList.add(State3); 
+		StateList.add(State4); 
+		StateList.add(State5); 
+		
+		
+		List<InstanceArtifact> ListOfLinks= new ArrayList<InstanceArtifact>(); 
+		ListOfLinks.add(link1); 
+		ListOfLinks.add(link2); 
+		ListOfLinks.add(link3); 
+		ListOfLinks.add(link4); 
+		ListOfLinks.add(link5); 
+		ListOfLinks.add(link6); 
+		ListOfLinks.add(link7); 
+		
+		List<InstanceArtifact> ListofCode= new ArrayList<InstanceArtifact>(); 
+		ListofCode.add(code1); 
+		ListofCode.add(code2); 
+		ListofCode.add(code3); 
+		ListofCode.add(code4); 
+		
+		List<InstanceArtifact> ListofStateMachines= new ArrayList<InstanceArtifact>(); 
+		ListofStateMachines.add(statemachine1); 
+		ListofStateMachines.add(statemachine2); 
+		ListofStateMachines.add(statemachine3); 
+		ListofStateMachines.add(statemachine4); 
+		
+		
+		List<String> RequirementLabels = new ArrayList<String>(); 
+		List<String> StateLabels = new ArrayList<String>(); 
+		List<String> CodeLabels = new ArrayList<String>(); 
+		List<String> StateMachineLabels = new ArrayList<String>(); 
+
+		
+		
+		
+		
+		
+		int i=1; 
+		
+	
+		for (InstanceArtifact ia: RequirementList) {
+			String name= "Requirement"+new Integer(i).toString(); 
+			RequirementLabels.add("Requirement"+i); 
+			i++; 
+		}
+		
+		
+		i=1; 
+		for (InstanceArtifact ia: StateList) {
+			StateLabels.add("State"+i); 
+			
+			i++; 
+		}
+	
+		i=1; 
+		for (InstanceArtifact ia: ListofCode) {
+			CodeLabels.add("Code"+i); 
+			
+			i++; 
+		}
+		
+		i=1; 
+		for (InstanceArtifact ia: ListofStateMachines) {
+			StateMachineLabels.add("StateMachine"+i); 
+			
+			i++; 
+		}
+		
+		
+	
+	System.out.println("ARTIFACT 1: "+ artifact1+ "artifact 2: "+artifact2);
+		
+		
+			
+			 String matrix[][] = new String[StateList.size()+1][RequirementList.size()+1];
+			   for (int k = 0; k < StateList.size(); k++) { 
+				   for (int j = 0; j < RequirementList.size(); j++) 
+				   { matrix[k][j]=" "; 
+				   }
+			   }
+
+				for( InstanceArtifact li: ListOfLinks) {
+					int y=0; 
+					for(InstanceArtifact re: ListofCode) {
+						int x=0; 
+						for(InstanceArtifact state: ListofStateMachines) {
+					
+						 if(li.getPropertyValue("linksource").equals(re) && li.getPropertyValue("linktarget").equals(state)) {   
+							 System.out.println("YEEEES AGAIN"+x+"     "+y);
+					        	matrix[x][y]= "*"; 
+					        	
+					        }
+						 else {
+							
+						 }
+						 x++; 
+						 
+					}    
+					y++; 
+				}
+				
+				
+				
+		 
+				}
+				
+	    	
+	    	
+				for(int p=0; p<StateList.size(); p++) {
+				  	  for(int m=0; m<RequirementList.size(); m++) {
+				  	  System.out.println("State: "+p+ "Requirement: "+ m+"   "+ matrix[p][m]);
+				  	  }
+				    }
+				
+				System.out.println("row count: "+mRowCount+ "column count: "+ mColumnCount);
+	        mGridBase = new GridBase(mRowCount, mColumnCount);
+	        ObservableList<ObservableList<SpreadsheetCell>> rows = FXCollections.observableArrayList();
+	        mSpreadsheetController = new SpreadsheetController(mGridBase);
+	    
+	        int indexColumn=0; 
+	      
+	        int indexRow=0; 
+	       
+	        for (int row = 0; row < mGridBase.getRowCount(); ++row) {
+	            final ObservableList<SpreadsheetCell> list = FXCollections.observableArrayList();    
+	            for (int column = 0; column < mGridBase.getColumnCount(); ++column) {
+	            	if(column<=CodeLabels.size() && row==0 && column>=1) {
+	            		 list.add(SpreadsheetCellType.STRING.createCell(0, column, 1, 1, CodeLabels.get(indexColumn)));
+	            		 indexColumn++; 
+	            	}
+	            	else if(column==0 && row<=StateMachineLabels.size() && row>=1) {
+	            		 list.add(SpreadsheetCellType.STRING.createCell(row, 0, 1, 1, StateMachineLabels.get(indexRow)));
+	            		 indexRow++; 
+	            	}
+	            	else  if(row<=ListofStateMachines.size() && column<=ListofCode.size()  && row>=1 && column>=1) {
+	            		System.out.println("ROW:  "+row+ "COLUMN: "+column);
+	            		int new_row= row-1;  
+	        			int new_column= column-1; 
+	        			System.out.println("STATE=======> "+ new_row+"  REQ=======> "+new_column);
+	        			System.out.println("MANTRIX ENTRY"+matrix[new_row][new_column]);
+	            		if((matrix[new_row][new_column]).equals("*")) {
+	            			
+	            			
+	                		list.add(SpreadsheetCellType.STRING.createCell(row, new_column, 1, 1, "*"));
+	                		
+	                	}
+	            		else {
+	                		list.add(SpreadsheetCellType.STRING.createCell(row, new_column, 1, 1, ""));
+	                	}
+	            		
+	            	}
+	            	
+	            		
+	  
+	            		
+	            	else {
+	            		
+	            			list.add(SpreadsheetCellType.STRING.createCell(row, column-1, 1, 1, " "));
+	            			
+	            		
+	            	}	
+	            	
+	                
+	            }
+	            
+	            
+	          
+	            
+	            
+	            
+	            rows.add(list);
+	        }
+	        
+
+	       
+	            
+	            
+			
+	        System.out.println("I AM HEREuuuuuu");
+	        
+	        mGridBase.setRows(rows);
+	        System.out.println("I AM HERE 2uuuuuu");
+	        mGridBase.addEventHandler(GridChange.GRID_CHANGE_EVENT, mSpreadsheetController);
+	        System.out.println("I AM HERE 3uuuuuuu");
+	        mSpreadsheet = new SpreadsheetView(mGridBase);
+	        System.out.println("I AM HERE 4uuuuuuuu");
+	        mSpreadsheet.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	        mSpreadsheet.addEventFilter(KeyEvent.KEY_RELEASED, e -> updateTextField());
+	        mSpreadsheet.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> updateTextField());
+	        for (SpreadsheetColumn c : mSpreadsheet.getColumns()) c.setPrefWidth(90);
+			
+			
+		}
     
 
 	private void updateTextField() {
